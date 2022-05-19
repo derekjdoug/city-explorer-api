6f7f1f50d834f462c2fbbc46e1dab4b94b3af3d3
Derek Douglas <derek.j.douglas13@gmail.com>
2022-05-18 22:11:12 -0500

    adds weather api functionality

--- server.js
+++ server.js
@@ -27,52 +27,55 @@ app.get('/', (request, response) => {
 
 app.get('/weatherData', async (request, response, next) => {
   try {
-    const city = request.query.city;
-    const url = `http://api.weatherbit.io/v2.0/current?lat=${lat}&lon=${lon}&key=${process.env.WEATHER_API_KEY}`;
-    console.log('type of query requested: ', city);
-    const weatherResults = new Forecast(city);
-    console.log('Forecast Result: ', weatherResults);
-    response.status(200).send(weatherResults);
+    const lat = request.query.lat;
+    const lon = request.query.lon;
+    console.log(request.query);
+    const url = `http://api.weatherbit.io/v2.0/forecast/daily?lat=${lat}&lon=${lon}&key=${process.env.WEATHER_API_KEY}`;
+    console.log(url);
+    const weatherResponse = await axios.get(url);
+    console.log(weatherResponse.data);
+    const weatherArr = weatherResponse.data.data.map(day => new Forecast(day));
+    console.log(weatherArr);
+    response.status(200).send(weatherArr);
   } catch (error) {
-      error.customMessage = 'Something went wrong in your weather API call.';
-      next(error);
+    error.customMessage = 'Something went wrong in your weather API call.';
+    next(error);
   }
 });
 
-app.get('/movieData', async (request, response, next) => {
-  try {
-  const searchQuery = request.query.searchQuery;
-  const url = `https://api.themoviedb.org/3/search/movie/${searchQuery}?api-key=${process.env.MOVIE_API_KEY}&`;
-  const movieResponse = await axios.get(url);
-  console.log(movieResponse.data);
-  const movieArr = movieResponse.data.results.map(movie => new Movies(movie));
-  response.status(200).send('testing on the movie endpoint');
-} catch (error) {
-  error.customMessage= 'Something went wrong in your weather API call.'
-  next(error);
-}
-});
+// app.get('/movieData', async (request, response, next) => {
+//   try {
+//   const searchQuery = request.query.searchQuery;
+//   const url = `https://api.themoviedb.org/3/search/movie/${searchQuery}?api-key=${process.env.MOVIE_API_KEY}&`;
+//   const movieResponse = await axios.get(url);
+//   console.log(movieResponse.data);
+//   const movieArr = movieResponse.data.results.map(movie => new Movies(movie));
+//   response.status(200).send('testing on the movie endpoint');
+// } catch (error) {
+//   error.customMessage= 'Something went wrong in your weather API call.'
+//   next(error);
+// }
+// });
 
 class Forecast {
-  static weatherForecast = require('./data/weather.json');
-
-  constructor(city) {
-    this.city = Forecast.weatherForecast.find(weatherObj => weatherObj.city_name.toLowerCase() === city.toLowerCase());
-    this.forecastArr = this.city.data.map(weatherObj => ({'date': weatherObj.datetime, 'description': weatherObj.weather.description}));
-    console.log(this.forecastArr);
-  }
-
-};
-
-class Movies {
-  constructor(movieObj) {
-
+  constructor(forecast) {
+    this.date = forecast.datetime;
+    this.description = forecast.weather.description;
+    // this.city = Forecast.weatherForecast.find(weatherObj => weatherObj.city_name.toLowerCase() === city.toLowerCase());
+    // this.forecastArr = this.city.data.map(weatherObj => ({'date': weatherObj.datetime, 'description': weatherObj.weather.description}));
+    console.log(this.forecast);
   }
 }
+
+// class Movies {
+//   constructor(movieObj) {
+
+//   }
+// }
 
 // Error handling function. MUST be last app.use function
 app.use((error, request, response, next) => {
   response.status(500).send(`Uh oh. Server error during API call: ${error.customMessage}`);
-})
+});
 // this turns the server on to the port that you specifed in your .env file
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
